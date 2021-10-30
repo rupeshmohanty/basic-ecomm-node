446f8bade863fd0907e59e44a001d2e1aadc3e03
Rupesh Chandra Mohanty <rupeshmohanty67@gmail.com>
2021-10-30 15:54:58 +0530

    Check commit

--- database.js
+++ database.js
@@ -1,3 +1,3 @@
-const uri = "mongodb+srv://bidishanayak:pass12345@cluster0.napdc.gcp.mongodb.net/ecomm?retryWrites=true&w=majority";
+const uri = "mongodb+srv://bidishanayak:pass12345@cluster0.b5m5g.mongodb.net/ecomm?retryWrites=true&w=majority";
 
 module.exports = uri;
